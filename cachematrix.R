6011149cdc3765862ad720964b74511c36e03444
Fabio Ciotoli <fabio.ciotoli@gmail.com>
2014-04-24 14:48:38 +0200

    Update cachematrix.R
    Commit of solution

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(z) {
+                x <<- z
+                m <<- NULL
+        }
+        get <- function() x
+        setsolution <- function(solution) m <<- solution
+        getsolution <- function() m
+        list(
+                set = set, 
+                get = get,
+                setsolution = setsolution,
+                getsolution = getsolution)
 
 }
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolution()
+        if (!is.null(m)) {
+                message("retrieving cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solution(data, ...)
+        x$setsolution(m)
+        m
 }
